8a6a8197fd390bcc72b5ceaa9caa0cc89d1e5965
lovansam <122586968+lovansam@users.noreply.github.com>
2023-03-29 15:28:45 +0700

    fix loading

--- src/ng2-pdfjs-viewer.component.ts
+++ src/ng2-pdfjs-viewer.component.ts
@@ -70,8 +70,8 @@ import { Component, Input, Output, ViewChild, EventEmitter, ElementRef } from '@
     position: relative;
     top: 0;
     left: 0;
-    width: 100vw;
-    height: 100vh;
+    width: 100%;
+    height: 100%;
     background-color: rgba(0, 0, 0, .25);
     z-index: 1000; 
   }
